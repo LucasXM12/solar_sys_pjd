0c8732af1dfbb98cc31b25845a5e3fb1162c83a6
Lucas de Oliveira Silva <lucasxm21@hotmail.com>
2017-10-13 14:04:38 -0300

    Ctrl of time scale

--- SistemaSolar/Assets/Assets/Scripts/FollowTarget.cs
+++ SistemaSolar/Assets/Assets/Scripts/FollowTarget.cs
@@ -6,6 +6,7 @@ public class FollowTarget: MonoBehaviour {
 	static public GameObject target;
 
 	public float step = 5f;
+	public float timeStep = 0.5f;
 
 	void Start() {
 		inc = Vector3.zero;
@@ -17,22 +18,29 @@ public class FollowTarget: MonoBehaviour {
 	void Update() {
 		if (target) {
 			inc[0] += this.step / 10 * Input.GetAxis("Horizontal");
+			inc[2] += this.step / 10 * Input.GetAxis("Vertical");
 
-			var change = this.step * Input.GetAxis("Mouse ScrollWheel");
 			var targetScale = target.transform.lossyScale.x;
-
 			var relativeY = 5 * targetScale;
 			var newY = relativeY + inc[1];
 
-			if (!(newY - change < relativeY))
-				inc[1] -= change;
+			var input = Input.GetAxis("Mouse ScrollWheel");
+			if (Input.GetKey(KeyCode.LeftControl)) {
+				float mult = input == 0 ? 0 : Mathf.Sign(input);
+				Time.timeScale += this.timeStep * mult;
+			} else {
+				var change = this.step * input;
 
-			inc[2] += this.step / 10 * Input.GetAxis("Vertical");
+				if (!(newY - change < relativeY))
+					inc[1] -= change;
+			}
 
 			var targetPos = target.transform.position;
 
 			transform.position = new Vector3(targetPos.x + inc[0],
 				newY, targetPos.z + inc[2]);
 		}
+
+		Debug.Log(Time.timeScale);
 	}
 }
